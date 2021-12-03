1db30877ccc31dde2114cd90103c8af4bc7e7792
beefybroccoli <tedkimemail@yahoo.com>
2021-12-02 23:39:40 -0800

    ...

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,6 @@
 // add middlewares here related to projects
+function verifyProject(req, res, next) {
+    
+}
+
+module.exports = { verifyProject };
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -2,9 +2,25 @@
 // Write your "actions" router here!
 const express = require("express");
 const router = express();
+const modelProject = require("./projects-model");
 
-router.get("/", (req,res)=>{
-    res.status(200).json({message:"hello from project-router"});
+router.get("/", async (req, res) => {
+  const projects = await modelProject.get();
+  res.status(200).json(projects);
+});
+
+router.get("/:id", async (req, res) => {
+  const projects = await modelProject.get();
+  res
+    .status(200)
+    .json(
+      projects.filter((element) => String(element.id) === String(req.params.id))
+    );
+});
+
+router.post("", async (req, res)=>{
+    
+    const newProject = await modelProject.insert();
 })
 
 module.exports = router;
