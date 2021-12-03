3f4f74bd2e3637e0ef58e057cfc5d8b67de18283
beefybroccoli <tedkimemail@yahoo.com>
2021-12-03 02:26:50 -0800

    met mvp

--- .history/api/middleware-errorhandler_20211203021351.js
+++
@@ -1,8 +0,0 @@
-function errorHandling(err, req, res, next) {
-    res.status(err.satus || 500).json({
-      message: `unknown error occured: ${err.message}`,
-      stack: err.stack,
-    });
-  }
-
-module.exports = err
