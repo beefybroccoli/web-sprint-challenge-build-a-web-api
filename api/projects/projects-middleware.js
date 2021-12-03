e1e11691a2aa21940fddf92428249c52219800b3
beefybroccoli <tedkimemail@yahoo.com>
2021-12-03 00:00:42 -0800

    ...

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,6 +1,32 @@
 // add middlewares here related to projects
-function verifyProject(req, res, next) {
-    
+const modelProject = require("./projects-model");
+function verifyNewProject(req, res, next) {
+  const { name, description } = req.body;
+  if (
+    !name ||
+    !description ||
+    name.trim().length === 0 ||
+    description.trim().length === 0
+  ) {
+    res.status(400).json({ message: "invalid name or description" });
+  } else {
+    req.newProject = { name, description, completed: false };
+    next();
+  }
 }
 
-module.exports = { verifyProject };
+async function verifyProjectId(req, res, next) {
+  try {
+    const project = await modelProject.get(req.params.id);
+    if (!project) {
+      res.status(404).json({ message: `project ${req.params.id} not found` });
+    } else {
+      req.project = project;
+      next();
+    }
+  } catch (err) {
+    res.status(500).json({ message: "error occured" });
+  }
+}
+
+module.exports = { verifyNewProject, verifyProjectId };
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -3,13 +3,14 @@
 const express = require("express");
 const router = express();
 const modelProject = require("./projects-model");
+const middlewareProject = require("./projects-middleware");
 
 router.get("/", async (req, res) => {
   const projects = await modelProject.get();
   res.status(200).json(projects);
 });
 
-router.get("/:id", async (req, res) => {
+router.get("/:id", middlewareProject.verifyProjectId, async (req, res) => {
   const projects = await modelProject.get();
   res
     .status(200)
@@ -18,9 +19,22 @@ router.get("/:id", async (req, res) => {
     );
 });
 
-router.post("", async (req, res)=>{
-    
-    const newProject = await modelProject.insert();
-})
+router.post("", middlewareProject.verifyNewProject, async (req, res) => {
+  const newProject = await modelProject.insert(req.newProject);
+  res.status(201).json(newProject);
+});
+
+router.delete("/:id", middlewareProject.verifyProjectId, async (req, res) => {
+  const result = await modelProject.remove(req.params.id);
+  if (result) {
+    res
+      .status(200)
+      .json({ message: `successfully deleted project ${req.params.id}` });
+  } else {
+    res
+      .status(400)
+      .json({ message: `fail to delete project ${req.params.id}` });
+  }
+});
 
 module.exports = router;
